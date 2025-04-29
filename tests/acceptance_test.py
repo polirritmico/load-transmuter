0eacc48ef6e7f962141474a13888abebb098b744
polirritmico <ebray187@gmail.com>
2025-04-29 00:31:02 -0400

    tests: correct indentation and skip acceptance test

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from src.google_sheets_importer import GoogleSheetsImporter
+# from src.google_sheets_importer import GoogleSheetsImporter
 from src.oracle8_handler import Oracle8DataBaseHandler
 from src.reports_resource import ReportResource
 from src.xlsx_importer import XlsxImporter
@@ -22,24 +22,26 @@ def oracledb():
     yield db_manager
     db_manager.close_db_connection(rollback=True)
 
-    def test_insert_and_load_db_data(oracle: Oracle8DataBaseHandler) -> None:
-        case = "tests/files/test_file.xlsx"
-        expected_value1 = "Something"
-        expected_value2 = "Some value"
 
-        resource = ReportResource("Some_data")
-        resource.set_input_handler(XlsxImporter())
-        resource.set_db_handler(oracle)
-        resource.load_input_data(case)
-        resource.validate_data()
-        resource.db_handler.connect_with_db()
-        resource.insert_data()
-        resource.load_db_data()
+# @pytest.mark.skip(reason="Not implemented")
+def test_insert_and_load_db_data(oracle: Oracle8DataBaseHandler) -> None:
+    case = "tests/files/test_file.xlsx"
+    expected_value1 = "Something"
+    expected_value2 = "Some value"
 
-        assert not resource.db_data.empty, "Empty response"
+    resource = ReportResource("Some_data")
+    resource.set_input_handler(XlsxImporter())
+    resource.set_db_handler(oracle)
+    resource.load_input_data(case)
+    resource.validate_data()
+    resource.db_handler.connect_with_db()
+    resource.insert_data()
+    resource.load_db_data()
 
-        output_value1 = resource.db_data["value1"].iloc[0]
-        output_value2 = resource.db_data["value2"].iloc[0]
+    assert not resource.db_data.empty, "Empty response"
 
-        assert expected_value1 == output_value1
-        assert expected_value2 == output_value2
+    output_value1 = resource.db_data["value1"].iloc[0]
+    output_value2 = resource.db_data["value2"].iloc[0]
+
+    assert expected_value1 == output_value1
+    assert expected_value2 == output_value2
