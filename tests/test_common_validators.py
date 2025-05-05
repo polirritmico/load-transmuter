b39274643412604706a69ee2d001eddd4f64843b
polirritmico <ebray187@gmail.com>
2025-05-04 21:56:07 -0400

    tests: use TEST_FILE in test validate extra spaces

--- tests/test_common_validators.py
+++ tests/test_common_validators.py
@@ -9,19 +9,17 @@ from tests.mock_classes import MockResource
 
 
 def test_validate_extra_spaces() -> None:
-    case_filename = "tests/files/common_validations.xlsx"
     case_section = "trailing_space"
     expected = (
         "Detected 1 problem(s):\n"
         "- Invalid last char: 'Trailing_space ' in column 'col_with_error' row '4'"
     )
 
+    resource = MockResource("mock")
+    resource.section = case_section
+    validator = CommonResourceFunctions(resource)
     importer = XlsxImporter()
-    mock_res = MockResource("mock")
-    mock_res.resource_name = case_filename
-    mock_res.section = case_section
-    validator = CommonResourceFunctions(mock_res)
-    mock_res.input_data = importer.load_data(case_filename, case_section)
+    resource.input_data = importer.load_data(TEST_FILE, case_section)
     with pytest.raises(ValueError) as error:
         validator.run_common_validations()
 
