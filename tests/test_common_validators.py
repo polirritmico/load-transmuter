d9761b583dd38fee105f9ba58945199ede9272df
polirritmico <ebray187@gmail.com>
2025-05-05 01:22:32 -0400

    tests: reorder tests and add test missing required value

--- tests/test_common_validators.py
+++ tests/test_common_validators.py
@@ -10,11 +10,59 @@ from tests.mock_classes import MockResource
 TEST_FILE: str = "tests/files/common_validations.xlsx"
 
 
-def test_validate_bad_characters() -> None:
-    case_section = "bad_character"
-    expected = "Detected 1 problem(s):\n- Invalid character in column 'col_c' row '2'."
+def test_missing_required_value() -> None:
+    case_section = "missing_required_value"
+    case_required_fields = ["col_a", "col_b"]
+    expected = (
+        "Detected 2 problem(s):\n"
+        "- Missing data in mandatory fields:\n"
+        "  - Column 'col_a':\n"
+        "    - Row 4\n"
+        "  - Column 'col_b':\n"
+        "    - Row 2\n"
+    )
 
+    resource = MockResource("mock")
+    resource.section = case_section
+    resource.required_fields = case_required_fields
+
+    validator = CommonResourceFunctions(resource)
     importer = XlsxImporter()
+    resource.input_data = importer.load_data(TEST_FILE, case_section)
+    with pytest.raises(ValueError) as error:
+        validator.run_common_validations()
+
+    output = error.value.args[0]
+    assert expected == output
+
+
+def test_missing_required_fields() -> None:
+    case_section = "missing_required_fields"
+    case_required_fields = ["col_c"]
+    expected = "Detected 1 problem(s):\n- Missing required field(s):\n   - col_c\n"
+
+    resource = MockResource("mock")
+    resource.section = case_section
+    resource.required_fields = case_required_fields
+
+    validator = CommonResourceFunctions(resource)
+    importer = XlsxImporter()
+    resource.input_data = importer.load_data(TEST_FILE, case_section)
+    with pytest.raises(ValueError) as error:
+        validator.run_common_validations()
+
+    output = error.value.args[0]
+    assert expected == output
+
+
+def test_validate_multiple_problems() -> None:
+    case_section = "multiple_problems"
+    expected = (
+        "Detected 2 problem(s):\n"
+        "- Invalid character in column 'col_a' row '2'.\n"
+        "- Invalid first char: ' leading space' in column 'col_b' row '3'."
+    )
+
     resource = MockResource("mock")
     resource.section = case_section
     validator = CommonResourceFunctions(resource)
@@ -46,35 +94,13 @@ def test_validate_extra_spaces() -> None:
     assert expected == output
 
 
-def test_validate_multiple_problems() -> None:
-    case_section = "multiple_problems"
-    expected = (
-        "Detected 2 problem(s):\n"
-        "- Invalid character in column 'col_a' row '2'.\n"
-        "- Invalid first char: ' leading space' in column 'col_b' row '3'."
-    )
+def test_validate_bad_characters() -> None:
+    case_section = "bad_character"
+    expected = "Detected 1 problem(s):\n- Invalid character in column 'col_c' row '2'."
 
-    resource = MockResource("mock")
-    resource.section = case_section
-    validator = CommonResourceFunctions(resource)
     importer = XlsxImporter()
-    resource.input_data = importer.load_data(TEST_FILE, case_section)
-    with pytest.raises(ValueError) as error:
-        validator.run_common_validations()
-
-    output = error.value.args[0]
-    assert expected == output
-
-
-def test_missing_required_fields() -> None:
-    case_section = "missing_required_fields"
-    case_required_fields = ["col_c"]
-    expected = "Detected 1 problem(s):\n- Missing required field(s):\n   - col_c\n"
-
     resource = MockResource("mock")
     resource.section = case_section
-    resource.required_fields = case_required_fields
-
     validator = CommonResourceFunctions(resource)
     importer = XlsxImporter()
     resource.input_data = importer.load_data(TEST_FILE, case_section)
